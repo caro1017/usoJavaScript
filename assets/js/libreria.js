b0887ebb0b6d6344d34debe319e267de14544abd
Carolina Uribe Botero <uribebotero.carolin@gmail.com>
2022-11-20 19:29:28 -0500

    cambio tiempo libreria

--- assets/js/libreria.js
+++ assets/js/libreria.js
@@ -12,7 +12,7 @@ document.querySelector('.btn_rotar').onclick = rotacion.restart;
 /* Animacion libreria greensock */
 
 var tl = gsap.timeline();
-tl.to(".box_1", {duration: 2, x: 300}) 
+tl.to(".box_1", {duration: 20, x: 300}) 
 .to(".box_2", {duration: 1, y: -100})
 .to(".box_3", {duration: 3, rotation: 360});
 
